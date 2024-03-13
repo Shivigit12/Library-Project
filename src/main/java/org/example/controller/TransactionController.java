c486a5c46fe4f07e10f20e6ac5404e46c82204ee
Shivam Kumar <shivam.singhs0807@gmail.com>
2024-03-13 22:22:32 +0530

    new changes added(Exception)

--- src/main/java/org/example/controller/TransactionController.java
+++ src/main/java/org/example/controller/TransactionController.java
@@ -1,14 +1,14 @@
 package org.example.controller;
 
+
 import org.example.entity.Transaction;
+import org.example.exception.CardNotFoundException;
+import org.example.exception.TransactionException;
 import org.example.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transact")
@@ -17,8 +17,18 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @GetMapping("/issueBook/{bookId}, {cardId}")
-    public ResponseEntity issueBook(@PathVariable int bookId, @PathVariable int cardId) {
+    public ResponseEntity<?> issueBook(@PathVariable int bookId, @PathVariable int cardId) throws TransactionException, CardNotFoundException {
         transactionService.issueBooks(bookId, cardId);
         return new ResponseEntity<>("Book has been issued",HttpStatus.OK);
+    }
+    @PostMapping("/returnBook/{bookId}/{cardId}")
+    public ResponseEntity<?> returnBook(@PathVariable int bookId, @PathVariable int cardId) throws TransactionException, CardNotFoundException {
+        transactionService.returnBook(bookId, cardId);
+        return new ResponseEntity<>("Book has been returned", HttpStatus.OK);
+    }
+    @GetMapping("/getDetails/{transactionId}")
+    public ResponseEntity<Transaction> getTransactionDetails(@PathVariable int id) throws TransactionException {
+        transactionService.getTransactionDetails(id);
+        return new ResponseEntity<Transaction>(HttpStatus.OK);
     }
 }
--- src/main/java/org/example/service/TransactionService.java
+++ src/main/java/org/example/service/TransactionService.java
@@ -2,20 +2,21 @@ package org.example.service;
 
 import org.example.entity.Book;
 import org.example.entity.Card;
-import org.example.entity.CardStatus;
+
 import org.example.entity.Transaction;
+import org.example.exception.CardNotFoundException;
+import org.example.exception.TransactionException;
 import org.example.repository.BookRepository;
 import org.example.repository.CardRepository;
 import org.example.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.auditing.CurrentDateTimeProvider;
+
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
 
-import static org.example.entity.CardStatus.ACTIVATED;
+
+
 import static org.example.entity.CardStatus.DEACTIVATED;
 
 @Service
@@ -28,15 +29,16 @@ public class TransactionService {
     TransactionRepository transactionRepository;
 
 
-    public void issueBooks(int bookId, int cardId) {
+    public void issueBooks(int bookId, int cardId) throws TransactionException, CardNotFoundException {
         Book book = bookRepository.findById(bookId)
-                .orElseThrow(()-> new RuntimeException("Book is not found"));
+                .orElseThrow(()-> new TransactionException("Book is not found"));
 
 
         Card card = cardRepository.findById(cardId)
-        .orElseThrow(()-> new RuntimeException("card id not found"));
+        .orElseThrow(()-> new TransactionException("card id not found"));
         if(card.getCardStatus() == DEACTIVATED)
-            throw new RuntimeException("Card is not active");
+            throw new CardNotFoundException("Card is not active");
+
 
         Transaction transaction = new Transaction();
         transaction.setBook(book);
@@ -53,4 +55,33 @@ public class TransactionService {
 
     }
 
+    public void returnBook(int bookId, int cardId) throws TransactionException, CardNotFoundException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(()-> new TransactionException("Book is not found"));
+        Card card = cardRepository.findById(cardId)
+                .orElseThrow(()-> new TransactionException("card id not found"));
+        if(card.getCardStatus() == DEACTIVATED)
+            throw new CardNotFoundException("Card is not active");
+        book.setAvailable(true);
+        LocalDate today = LocalDate.now();
+
+        Transaction transaction = new Transaction();
+        if(today.isAfter(transaction.getBookDueDate())) {
+            int daysOverDue = (int) (today.toEpochDay() - transaction.getBookDueDate().toEpochDay());
+            int fine = 20 * daysOverDue;
+            transaction.setFineAmount(fine);
+        }
+
+        transaction.setBook(book);
+        transaction.setIssued(false);
+        transaction.setCard(null);
+        transaction.setReturned(true);
+        transaction.setStatus("book is returned");
+        transactionRepository.save(transaction);
+    }
+
+    public Transaction getTransactionDetails(int id) throws TransactionException {
+        return transactionRepository.findById(id)
+                .orElseThrow(()-> new TransactionException("Transaction id is not found"));
+    }
 }
